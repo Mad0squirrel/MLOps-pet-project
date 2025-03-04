4424a34b549ac7f940457362a475df701cdbd9d1
Mad0Squirrel <eghorkin@gmail.com>
2025-03-04 14:13:24 +0300

    fix map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
-import ApplicationMap from "./components/application-map/ApplicationMap.tsx";
+import maplibregl from 'maplibre-gl';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <ApplicationMap/>
-  )
-}
+const App = () => {
+    useEffect(() => {
+        const map = new maplibregl.Map({
+            container: 'map',
+            style: 'https://api.maptiler.com/maps/basic-v2/style.json?key=bLQ9PLUtNvDYbN6lEQDA',
+            center: [37.6173, 55.7558],
+            zoom: 10
+        });
 
-export default App
+        return () => map.remove();
+    }, []);
+
+    return <div id="map" style={{ width: "100vw", height: "100vh" }} />;
+};
+
+export default App;
+
